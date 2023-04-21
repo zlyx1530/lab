585d25f913db80f041f16123ed7174d5934af22f
Zlyx <zallanin@hotmail.com>
2023-04-21 18:59:07 -0500

    added account.py

---
+++ account.py
@@ -0,0 +1,24 @@
+class Account:
+    def __init__(self, name):
+        self.__account_name = name
+        self.__account_balance = 0
+
+    def deposi(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            return True
+        else:
+            return False
+
+    def withdraw(self, amount):
+        if amount > self.__account_balance or amount <= 0:
+            return False
+        else:
+            self.__account_balance += amount
+            return True
+
+    def get_balance(self):
+        return self.__account_balance
+
+    def get_name(self):
+        return self.__account_name
